04932a464539eebc60642dc441a36a1618d3251b
Nagibatorka1488228 <92184449+Nagibatorka1488228@users.noreply.github.com>
2021-12-09 15:32:34 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+def sort_sentence(s):
+l = s.split(' ')
+l = [x.lower() for x in sorted(l, key=len)]
+l[0] = l[0].capitalize()
+s = ' '.join(l)
+return s
+
+if __name__ == "__main__":
+assert sort_sentence('Keep calm and carry on') == 'On and keep calm carry'
+print('OK')
